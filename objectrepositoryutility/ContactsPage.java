2be859178df1abe5897f580bc1526b5a9e9a987e
Sadhurya30 <132685899+Sadhurya30@users.noreply.github.com>
2024-09-29 18:11:05 +0530

    Add files via upload

---
+++ objectrepositoryutility/ContactsPage.java
@@ -0,0 +1,59 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ContactsPage {
+
+	WebDriver driver; 
+	public ContactsPage(WebDriver driver) {
+	    this.driver=driver; 
+	    PageFactory.initElements(driver,this);
+   	}
+	
+	@FindBy(linkText = "Contacts")
+	private WebElement contacts;
+	
+	
+
+	@FindBy(xpath="//img[@title='Create Contact...']")
+	private WebElement createNewContactBtn;
+	
+	@FindBy(name = "search_text")
+	private WebElement searchEditContact; 
+	
+	@FindBy(id="bas_searchfield")
+	private WebElement searchContactDD;
+	
+	@FindBy(name = "submit")
+	private WebElement searchBtnContact;
+	
+	
+	
+	public WebElement getCreateNewContactBtn() {
+		return createNewContactBtn;
+	}
+
+	public WebElement getContacts() {
+		return contacts;
+	}
+
+	public WebElement getSearchEditContact() {
+		return searchEditContact;
+	}
+
+	public WebElement getSearchContactDD() {
+		return searchContactDD;
+	}
+
+	public WebElement getSearchBtnContact() {
+		return searchBtnContact;
+	}
+	
+	
+	
+}
